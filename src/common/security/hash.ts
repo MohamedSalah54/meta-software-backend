015b945944f7009e006d7c319a1c1c431f76c594
MohamedSalah54 <mohamed.salah199988.com>
2026-01-28 17:12:29 +0200

    fixes for packages

--- src/common/security/hash.ts
+++ src/common/security/hash.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 export function hash(data: string, saltOrRound: number = 10): string {
   return bcrypt.hashSync(data, saltOrRound);
